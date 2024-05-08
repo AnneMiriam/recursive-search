6e63b4a6e916b88e1214d50c9bd7dcf45351d16d
Anne Hastings <anne.miriam@gmail.com>
2024-05-08 16:10:58 -0600

    passed tests

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,8 +1,22 @@
 function recursiveSearch(arr, target) {
-  // type your code here
+  // use the array.includes method to find if target is in array
+  if (arr.includes(target)) {
+    return true
+  } else {
+    return false
+  }
 }
 
 if (require.main === module) {
+  //  my own tests
+  // console.log("Expecting: false");
+  // console.log("=>", recursiveSearch([1, 3, 5, 7, 9, 11, 12], 2));
+
+  // console.log("");
+
+  // console.log("Expecting: true");
+  // console.log("=>", recursiveSearch([2, 4, 6, 8, 10, 12], 10));
+
   // add your own tests in here
   console.log("Expecting: true");
   console.log("=>", recursiveSearch([1, 2, 3], 2));
